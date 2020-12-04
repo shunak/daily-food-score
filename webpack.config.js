65249d4947275999ef0b715f27a17db8214dcbc8
shunak <shuna966@gmail.com>
2020-12-04 16:55:34 +0900

    introduce ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
 console.log(__dirname);
 module.exports = {
-    entry: './dist/main.js',
+    entry: './src/app/main.ts',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname,'dist')
     },
-    devtool: 'inline-source-map'
+    devtool: 'inline-source-map',  
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
 }
